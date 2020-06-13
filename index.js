46d1e94680d896b1c5540a65fd83e37933e3d59f
terenceteo <terenceteoly@gmail.com>
2020-06-13 22:47:47 +0800

    First git commit. This provides a barebone template for socket.io apps

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+    socket.on('joinRoom', () => {
+        console.log(socket.id);
+    });  
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, (err) => {
+    console.log(`Server running on port ${PORT}...`);
+});
