f93b88ffb1afff3c21a71ac11c6d2b28c72656a5
terenceteo <terenceteoly@gmail.com>
2020-06-21 21:48:18 +0800

    Remove need for file extension for static file

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
-const path = require('path');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  express.static(path.join(__dirname, "public"), { extensions: ["html"] })
+);
 
-io.on('connection', socket => {
-    socket.on('joinRoom', () => {
-        console.log(socket.id);
-    });  
+io.on("connection", (socket) => {
+  socket.on("joinRoom", () => {
+    console.log(socket.id);
+  });
 });
 
 // Start server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, (err) => {
-    console.log(`Server running on port ${PORT}...`);
+  console.log(`Server running on port ${PORT}...`);
 });
